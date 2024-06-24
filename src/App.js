2b8a1eadfbe67089b9f53493826911e76c9a44a4
HaniCeebla <hanicabdillahi3@gmail.com>
2024-06-24 20:17:46 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -123,7 +123,7 @@ function App() {
                 <div className="section-wrapper pl-60 pr-60 pt-60">
                     <div className="bostami-page-title-wrap mb-15">
                         <h2 className="page-title">about</h2>
-                        <p>Highly skilled and motivated software developer with1 year of experience in designing, developing, anddeploying innovative software solutions. Passionateabout leveraging cutting-edge technologies to createrobust and scalable applications that address real-world challenges.</p>
+                        <p>Highly skilled and motivated software developer with 2 year of experience in designing, developing, anddeploying innovative software solutions. Passionateabout leveraging cutting-edge technologies to createrobust and scalable applications that address real-world challenges.</p>
                      
                     </div>
                 </div>
