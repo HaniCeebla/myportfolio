1df8dabec060fe339a6b6ea0d7dc978568a812ec
HaniCeebla <hanicabdillahi3@gmail.com>
2024-06-24 09:43:41 +0300

    boostraped

--- src/App.js
+++ src/App.js
@@ -1,24 +1,340 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { FaCalendarWeek, FaEnvelope, FaFacebook, FaLinkedin, FaLocationArrow, FaPhone, FaWhatsapp } from 'react-icons/fa';
+import './App.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+     <div className="page-wrapper home-3" data-background="/img/bg/page-bg-1.jpg">
+
+
+{/* <div id="preloader">
+    <div className="loader_line"></div>
+</div> */}
+
+
+
+
+
+<div className="container z-index-3">
+    <div className="row">
+
+   
+        <div className="col-xxl-4 col-xl-4 col-lg-4">
+            <div className="bostami-parsonal-info-area">
+                <div className="bostami-parsonal-info-wrap">
+
+               
+                    <div className="bostami-parsonal-info-img">
+                        <img src="/img/parsonal-info/parson-img-3.png" alt="avatar"/>
+                    </div>
+
+                  
+                    <h4 className="bostami-parsonal-info-name">
+                        <a href="#">Monalisa Ashley</a>
+                    </h4>
+                    <span className="bostami-parsonal-info-bio mb-15">Ui/Ux Designer</span>
+
+                 
+                    <ul className="bostami-parsonal-info-social-link mb-30">
+                        <li>
+                            <a href="#" className="facebook">
+                                <i className="fa-brands "><FaFacebook/></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="twitter">
+                            <i className="fa-brands "><FaLinkedin/></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="instagram">
+                            <i className="fa-brands "><FaWhatsapp/></i>
+                            </a>
+                        </li>
+                      
+                    </ul>
+
+                
+                    <div className="bostami-parsonal-info-contact mb-30">
+                        <div className="bostami-parsonal-info-contact-item phone">
+                            <div className="icon">
+                                <i className="fa-solid fa-mobile-screen-button"><FaPhone/></i>
+                            </div>
+                            <div className="text">
+                                <span>Phone</span>
+                                <p>+252 63 3994689</p>
+                            </div>
+                        </div>
+
+
+                        <div className="bostami-parsonal-info-contact-item email">
+                            <div className="icon">
+                                <i className="fa-regular fa-envelope-open-text"><FaEnvelope/></i>
+                            </div>
+                            <div className="text">
+                                <span>Email</span>
+                                <p>hanicabdillahi@gmail.com</p>
+                            </div>
+                        </div>
+
+                        <div className="bostami-parsonal-info-contact-item location">
+                            <div className="icon">
+                                <i className="fa-solid fa-location-dot"><FaLocationArrow/></i>
+                            </div>
+                            <div className="text">
+                                <span>Location</span>
+                                <p>Hargeisa</p>
+                            </div>
+                        </div>
+
+                        <div className="bostami-parsonal-info-contact-item calendar">
+                            <div className="icon">
+                                <i className="fa-light fa-calendar-days"><FaCalendarWeek/></i>
+                            </div>
+                            <div className="text">
+                                <span>Year of Experiences</span>
+                                <p>2+</p>
+                            </div>
+                        </div>
+                    </div>
+
+                   
+                    <div className="bostami-parsonal-info-btn">
+                        <a className="btn-2 circle" href="#">
+                            <span className="icon">
+                                <i className="fa-regular fa-download"></i>
+                            </span>
+                            download cv
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+      
+        <div className="col-xxl-8 col-xl-8 col-lg-8">
+
+         
+         
+
+            <div className="bostami-page-content-wrap">
+
+                
+                <div className="section-wrapper pl-60 pr-60 pt-60">
+                    <div className="bostami-page-title-wrap mb-15">
+                        <h2 className="page-title">about</h2>
+                        <p>Highly skilled and motivated software developer with1 year of experience in designing, developing, anddeploying innovative software solutions. Passionateabout leveraging cutting-edge technologies to createrobust and scalable applications that address real-world challenges.</p>
+                     
+                    </div>
+                </div>
+
+             
+                <div className="section-wrapper pl-60 pr-60">
+
+                    <div className="bostami-section-title-wrap mt-30 mb-20">
+                        <h3 className="section-title">What I do!</h3>
+                    </div>
+
+                    <div className="bostami-what-do-wrap mb-30">
+                        <div className="row">
+
+                            <div className="col-xxl-6 col-xl-6 col-lg-6">
+                                <div className="bostami-what-do-item bg-prink">
+                                    <div className="icon">
+                                        <i className="fa-light fa-swatchbook"></i>
+                                    </div>
+                                    <div className="text">
+                                        <h4 className="title">Ui/Ux Design</h4>
+                                        {/* <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+                                            euismod tincidunt volutpat.</p> */}
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div className="col-xxl-6 col-xl-6 col-lg-6">
+                                <div className="bostami-what-do-item bg-catkrill">
+                                    <div className="icon">
+                                        <i className="fa-regular fa-grid-2"></i>
+                                    </div>
+                                    <div className="text">
+                                        <h4 className="title">software Development</h4>
+                                        {/* <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+                                            euismod tincidunt volutpat.</p> */}
+                                    </div>
+                                </div>
+                            </div>
+                          
+                            <div className="col-xxl-6 col-xl-6 col-lg-6">
+                                <div className="bostami-what-do-item  bg-catkrill">
+                                    <div className="icon">
+                                        <i className="fa-regular fa-camera-retro"></i>
+                                    </div>
+                                    <div className="text">
+                                        <h4 className="title">Web Design</h4>
+                                        {/* <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+                                            euismod tincidunt volutpat.</p> */}
+                                    </div>
+                                </div>
+                            </div>
+                       
+                            <div className="col-xxl-6 col-xl-6 col-lg-6">
+                                <div className="bostami-what-do-item bg-prink bg-blue">
+                                    <div className="icon">
+                                        <i className="fa-regular fa-code"></i>
+                                    </div>
+                                    <div className="text">
+                                        <h4 className="title">Web Development</h4>
+                                        {/* <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+                                            euismod tincidunt volutpat.</p> */}
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="bostami-page-content-wrap">
+
+
+<div className="section-wrapper pl-60 pr-60 pt-60">
+    <div className="bostami-page-title-wrap mb-15">
+        <h2 className="page-title">Portfolio</h2>
     </div>
+</div>
+
+
+<div className="section-wrapper pr-60 pl-60 mb-60">
+    <div className="row" >
+
+      
+
+        <div className="col-12" >
+            <div id="fillter-item-active" className="fillter-item-wrap " >
+                <div className="grid-sizer"></div>
+
+          
+            
+
+
+          
+                <div className='porto'>
+                  <div className=" isotop-item mockup">
+                      <div className="fillter-item bg-prink">
+                          <a className="img" href="https://rooyaljet.com/" data-bs-toggle="modal"
+                              data-bs-target="#portfolio-1">
+                              <img src="/img/work/work-img-7.png" style={{height:"10rem",objectFit:"cover"}} alt=""/>
+                          </a>
+                          <span className="item-subtitle">Web design | development </span>
+                          <h6 className="item-title">
+                              <a   href="https://rooyaljet.com/" data-bs-toggle="modal"
+                                  data-bs-target="#portfolio-1">Rooyel jet</a>
+                          </h6>
+                      </div>
+                  </div>
+                  <div className=" isotop-item mockup">
+                      <div className="fillter-item bg-prink">
+                          <a className="img" href="https://www.greenpowerre.com/" data-bs-toggle="modal"
+                              data-bs-target="#portfolio-1">
+                              <img src="/img/work/work-img-9.png" style={{height:"10rem",objectFit:"fit"}} alt=""/>
+                          </a>
+                          <span className="item-subtitle">Web design | development </span>
+                          <h6 className="item-title">
+                              <a  href="https://www.greenpowerre.com/" data-bs-toggle="modal"
+                                  data-bs-target="#portfolio-1">Greenpower</a>
+                          </h6>
+                      </div>
+                  </div>
+                  <div className=" isotop-item mockup">
+                      <div className="fillter-item bg-prink">
+                          <a className="img" href="https://www.ccclr1.org/" data-bs-toggle="modal"
+                              data-bs-target="#portfolio-1">
+                              <img src="/img/work/work-img-8.png" alt="" style={{height:"10rem",objectFit:"cover"}}/>
+                          </a>
+                          <span className="item-subtitle">Web design | development </span>
+                          <h6 className="item-title">
+                              <a href="https://www.ccclr1.org/" data-bs-toggle="modal"
+                                  data-bs-target="#portfolio-1">CCCLR</a>
+                          </h6>
+                      </div>
+                  </div>
+                  
+                </div>
+
+          
+         
+
+            
+               
+
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+
+</div>
+
+            
+                <div className="section-wrapper bg-light-white-2 pt-45 pb-60 pl-60 pr-60">
+
+                    <div className="bostami-section-title-wrap text-center mb-50">
+                        <h3 className="section-title">clinet</h3>
+                    </div>
+
+                    <div className="bostami-client-slider">
+                        <div className="swiper-container client_slide_active">
+                            <div className="swiper-wrapper porto2" >
+
+                             
+                                <div className="swiper-slide">
+                                    <img className="bostami-client-slider-logo"
+                                        src="/img/client/client-logo-1.png" alt=""/>
+                                </div>
+
+                                <div className="swiper-slide">
+                                    <img className="bostami-client-slider-logo"
+                                        src="/img/client/client-logo-2.png" alt=""/>
+                                </div>
+
+                              
+                                <div className="swiper-slide">
+                                    <img className="bostami-client-slider-logo"
+                                        src="/img/client/client-logo-3.png" alt=""/>
+                                </div>
+
+                              
+                                <div className="swiper-slide">
+                                    <img className="bostami-client-slider-logo"
+                                        src="/img/client/client-logo-4.png" alt=""/>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+             
+                <div className="footer-copyright text-center pt-25 pb-25">
+                    <span>© 2024 All Rights Reserved.</span>
+                </div>
+
+            </div>
+        </div>
+       
+
+    </div>
+
+
+</div>
+
+
+</div>
+   </div>
   );
 }
 
