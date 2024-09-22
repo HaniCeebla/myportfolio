5fafe1a88bdbab7efd033f2f4be74969c1fda00e
HaniCeebla <hanicabdillahi3@gmail.com>
2024-09-22 15:31:25 +0300

    update the README.md with link to contributing guide

--- src/App.js
+++ src/App.js
@@ -124,7 +124,7 @@ function App() {
                   <div className="bostami-parsonal-info-btn">
                     <a
                       className="btn-2 circle"
-                      href="https://drive.google.com/file/d/1VMc2D5Ay_haWuQXfPrT_MeIvRAXB52VZ/view?usp=drive_link"
+                      href="https://drive.google.com/file/d/1tf2T2JpGWpca-OrFaSAS1cr93QL2LBAB/view?usp=drive_link"
                     >
                       <span className="icon">
                         <i className="fa-regular fa-download"></i>
